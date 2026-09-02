e993ff363f57a889014bc2a0f9a660c4624eb1c4
Yang.LUO <992534544@qq.com>
2026-09-02 19:46:14 +0800

    build: finalize EtherCAT find module

--- cmake/FindEtherCAT.cmake
+++ cmake/FindEtherCAT.cmake
@@ -1,3 +1,8 @@
+# Copyright (c) 2026 'Yang Luo, luoyang@sia.cn'
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 find_path(EtherCAT_INCLUDE_DIR
     NAMES ecrt.h
     HINTS ENV ETHERCAT_ROOT
